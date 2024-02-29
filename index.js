b6cd3bc1636b87d469eed6a669994d6e1a88b3b5
Jose Diaz <jose.diaz.roviralta@outlook.es>
2024-02-29 21:19:49 +0100

    js to main

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { 'default': mod }
 }
-// eslint-disable-next-line no-undef
 Object.defineProperty(exports, '__esModule', { value: true })
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const express_1 = __importDefault(require('express'))
@@ -14,25 +13,27 @@ const app = (0, express_1.default)()
 app.use(express_1.default.json())
 app.use((0, cors_1.default)())
 app.use(loggerMiddleware_1.logger)
-const PORT = 3001
-let notes = [{
-    'id': 1,
-    'content': 'Mi cumpleaños ',
-    'date': '19:00-26/4/2024',
-    'important': false
-},
-{
-    'id': 2,
-    'content': 'Tengo que estudiar ',
-    'date': '19:00-20/8/2023',
-    'important': true
-},
-{
-    'id': 3,
-    'content': 'Ir en skate ',
-    'date': '10:00-25/9/2023',
-    'important': false
-}]
+const PORT = process.env.PORT || 3001
+let notes = [
+    {
+        id: 1,
+        content: 'Mi cumpleaños ',
+        date: '19:00-26/4/2024',
+        important: false
+    },
+    {
+        id: 2,
+        content: 'Tengo que estudiar ',
+        date: '19:00-20/8/2023',
+        important: true
+    },
+    {
+        id: 3,
+        content: 'Ir en skate ',
+        date: '10:00-25/9/2023',
+        important: false
+    }
+]
 app.get('/', (request, response) => {
     // express detecta que el contenido es HTML y lo insertara como tal en la web
     response.send('<h1>Hello Worlds</h1>')
@@ -45,14 +46,14 @@ app.get('/api/notes/:id', (request, response, next) => {
     // coge la direccion que le añadimos despues de notes: /api/notes/1 guarda 1 en const id
     const id = Number(request.params.id)
     // si hay una nota con id uno la guarda en const note
-    const note = notes.find(note => note.id === id)
+    const note = notes.find((note) => note.id === id)
     if (note) {
         // si encuentra la id de la nota la muestra
         response.json(note)
     }
     else {
         // Si no la encuentra muestra un error de que no se ha encoontrado la pagina web
-        // response.status(404).end() 
+        // response.status(404).end()
         // I dissabled the line above so it can go to the error app.use below
         next()
     }
@@ -60,7 +61,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 // DELETE----------------------------------------DELETE----------------------------------------DELETE
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    notes = notes.filter((note) => note.id !== id)
     response.status(204).json('Post deleted').end()
 })
 // POST------------------------------------------POST------------------------------------------POST
@@ -72,7 +73,7 @@ app.post('/api/notes', (request, response) => {
             error: 'note.content is missing'
         })
     }
-    const ids = notes.map(note => note.id)
+    const ids = notes.map((note) => note.id)
     const maxId = Math.max(...ids)
     const newNote = {
         id: maxId + 1,
